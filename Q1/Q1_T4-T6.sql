772e977d6049555331cd74c3118ba5c39c38ab01
abradley1984 <abradley1984@gmail.com>
2022-03-24 12:30:48 -0400

    Q1_T4_t6
    updating file extension

--- Q1/Q1_T4-T6.sql
+++ Q1/Q1_T4-T6.sql
@@ -1,44 +1,14 @@
---Q2_T4.csv
-with joined1 as (select *
-                 from SHTG_Q2_STEP1_d5
-                          join SHTG_Q2_STEP3_d1 using (patid)
-                 where cohort is not null),
-     pat_list as
-         (
-             select distinct *
 
-             from joined1
-         )
-        
-     select count(distinct patid) as N, TG_category, LDL_category2 from pat_list
+
+ --Q1_T4.csv
+with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
+select count(distinct patid) as N, TG_category, LDL_category2 from pat_list
 group by TG_category, LDL_category2;
---Q2_T5.csv
-with joined1 as (select *
-                 from SHTG_Q2_STEP1_d5
-                          join SHTG_Q2_STEP3_d1 using (patid)
-                 where cohort is not null),
-     pat_list as
-         (
-             select distinct *
-
-             from joined1
-         )
-        
-select count(distinct patid) as N, TG_category, NHDL_category2
-from pat_list
+--Q1_T5.csv
+with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
+select count(distinct patid) as N, TG_category, NHDL_category2 from pat_list
 group by TG_category, NHDL_category2;
---Q2_T6.csv
-with joined1 as (select *
-                 from SHTG_Q2_STEP1_d5
-                          join SHTG_Q2_STEP3_d1 using (patid)
-                 where cohort is not null),
-     pat_list as
-         (
-             select distinct *
-
-             from joined1
-         )
-        
-select count(distinct patid) as N, LDL_category2, NHDL_category2
-from pat_list
-group by LDL_category2, NHDL_category2;
+--Q1_T6.csv
+with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
+select count(distinct patid) as N,  LDL_category2, NHDL_category2 from pat_list
+group by  LDL_category2, NHDL_category2;
