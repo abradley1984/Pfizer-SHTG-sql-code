8c2d3f32d0b8f9c07772b78bda82f73d8464444f
abradley1984 <abradley1984@gmail.com>
2022-03-28 14:26:28 -0400

    editing t4-t6

--- Q1 oracle/Q1_T4-T6.sql
+++ Q1 oracle/Q1_T4-T6.sql
@@ -1,14 +1,27 @@
-
+--Run time: <1 minute
+--3 tables are output and should be saved to csv
 
  --Q1_T4.csv
 with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
 select count(distinct patid) as N, TG_category, LDL_category2 from pat_list
-group by TG_category, LDL_category2;
+group by TG_category, LDL_category2
+union select count(distinct patid) as N, 'LDL_total', LDL_category2 from pat_list
+group by  LDL_category2
+union select count(distinct patid) as N, 'Tg_total', TG_category from pat_list
+group by  TG_category ;
 --Q1_T5.csv
 with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
 select count(distinct patid) as N, TG_category, NHDL_category2 from pat_list
-group by TG_category, NHDL_category2;
+group by TG_category, NHDL_category2
+union select count(distinct patid) as N, 'NHDL_total',  NHDL_category2 from pat_list
+group by  NHDL_category2
+union select count(distinct patid) as N, 'Tg_total', TG_category from pat_list
+group by  TG_category ;
 --Q1_T6.csv
 with pat_list as (select * from shtg_Q1_cohorts_with_exclusions)
 select count(distinct patid) as N,  LDL_category2, NHDL_category2 from pat_list
-group by  LDL_category2, NHDL_category2;
+group by  LDL_category2, NHDL_category2
+union select count(distinct patid) as N, 'NHDL_total',  NHDL_category2 from pat_list
+group by  NHDL_category2
+union select count(distinct patid) as N, 'LDL_total', LDL_category2 from pat_list
+group by  LDL_category2;
