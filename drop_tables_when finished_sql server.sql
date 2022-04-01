d8dd5b3d2d7b77783606de74eb7b782d41473ae6
abradley1984 <abradley1984@gmail.com>
2022-04-01 10:56:35 -0400

    adding code for dropping tables

---
+++ drop_tables_when finished_sql server.sql
@@ -0,0 +1,33 @@
+--This code will drop all of the tables written to disk once all of the queries are totally finished.
+-- syntax is for sql server
+-- I would leave them for now, if you don't need the space, since we may need to edit to fix bugs etc.
+
+--Q1:
+IF OBJECT_ID(SHTG_COHORT_DEFINITION) IS NOT NULL
+    DROP TABLE SHTG_COHORT_DEFINITION;
+IF OBJECT_ID(shtg_q1_total_counts) IS NOT NULL
+    DROP TABLE shtg_q1_total_counts;
+IF OBJECT_ID(SHTG_Q1_COHORTS_WITH_EXCLUSIONS) IS NOT NULL
+    DROP TABLE SHTG_Q1_COHORTS_WITH_EXCLUSIONS;
+IF OBJECT_ID(SHTG_Q1_COHORTS_WITH_EX) IS NOT NULL
+    DROP TABLE SHTG_Q1_COHORTS_WITH_EX;
+IF OBJECT_ID(Q1_Labs_all) IS NOT NULL
+    DROP TABLE Q1_Labs_all;
+
+
+Q2:
+
+
+IF OBJECT_ID(SHTG_Q2_Step1_D5) IS NOT NULL
+    DROP TABLE SHTG_Q2_Step1_D5;
+IF OBJECT_ID(SHTG_Q2_STEP3_D5) IS NOT NULL
+    DROP TABLE SHTG_Q2_STEP3_D5;
+IF OBJECT_ID(Q2_Labs_all) IS NOT NULL
+    DROP TABLE Q2_Labs_all;
+IF OBJECT_ID(SHTG_MEDS_Q2) IS NOT NULL
+    DROP TABLE SHTG_MEDS_Q2;
+IF OBJECT_ID(SHTG_Q2_Step1) IS NOT NULL
+    DROP TABLE SHTG_Q2_Step1;
+
+IF OBJECT_ID(SHTG_Q2_STEP3) IS NOT NULL
+    DROP TABLE SHTG_Q2_STEP3;
